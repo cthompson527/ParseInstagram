887f460858a12a873ec5f129ba4282585d0e572a
Cory Thompson <cory.thompson527@tamu.edu>
2016-03-06 00:33:47 -0600

    commiting for turnin

---
+++ ParseInstagram/Picture.swift
@@ -0,0 +1,13 @@
+//
+//  Picture.swift
+//  ParseInstagram
+//
+//  Created by Cory Thompson on 3/4/16.
+//  Copyright © 2016 Cory Thompson. All rights reserved.
+//
+
+import UIKit
+
+class Picture: NSObject {
+
+}
---
+++ ParseInstagram/PictureCell.swift
@@ -0,0 +1,24 @@
+//
+//  PictureCell.swift
+//  ParseInstagram
+//
+//  Created by Cory Thompson on 3/4/16.
+//  Copyright © 2016 Cory Thompson. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
